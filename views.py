447cb7e0ed5587223f25018762d3293233ac09b4
cvp5380 <49172372+cvp5380@users.noreply.github.com>
2019-04-21 23:36:50 -0400

    views ch6

--- views.py
+++ views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+rom django.shortcuts import redirect, render
 
-# Create your views here.
-home_page = None
+from lists.models import Item
 
-def home_page(request):
-    pass
+
 
 def home_page(request):
-    return HttpResponse('<html><title>Playoffs</title>></html>')
+
+    if request.method == 'POST':
+
+        Item.objects.create(text=request.POST['item_text'])
+
+        return redirect('/')
+
+
+
+    items = Item.objects.all()
+
+    return render(request, 'home.html', {'items': items})
